eb75a1785602434dcaed35b55aca15390ef3747c
Aminu Rabiu <arabiu033@gmail.com>
2022-09-13 00:56:36 +0100

    done with the html and css for the mobile version

---
+++ script.js
@@ -0,0 +1,26 @@
+const speakers = [
+  {
+    imgSrc: './assets/mobile/kilnam.png',
+    speakerName: 'Yochai Benkler',
+    speakerField: 'Berkan Professor of History with over 20 years experience in podcast',
+    speakerDesc: 'Benkler studies commons-based peer production and published his seminal book Journey to the podcasting world.'
+  },
+  {
+    imgSrc: './assets/mobile/yochai.png',
+    speakerName: 'Kilnam Chon',
+    speakerField: '',
+    speakerDesc: 'Kilnam Chon helped bring the podcast submit to Asia and is an outspoken advocate for open podcast and digital commons'
+  },
+  {
+    imgSrc: './assets/mobile/sohyeong.png',
+    speakerName: 'SohYeong Noh',
+    speakerField: 'Director of Art Center and a board member of PD Podcasting',
+    speakerDesc: 'As the main venue for new media art production in korea, Nabi promotes cross-displinary collaboration and understanding'
+  },
+  {
+    imgSrc: './assets/mobile/julia.png',
+    speakerName: 'Julia Leda',
+    speakerField: 'President of young pirates of Europe',
+    speakerDesc: ''
+  }
+]
